e3b112c503b6742d3fb8d7107e0094395c11eb0a
titawidia <gitawidia166@gmail.com>
2023-01-20 15:11:26 +0700

    Tambah Pendidikan

---
+++ src/main/java/com/itc/demobackend/repository/PendidikanRepository.java
@@ -0,0 +1,7 @@
+package com.itc.demobackend.repository;
+
+import com.itc.demobackend.entity.Pendidikan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PendidikanRepository extends JpaRepository<Pendidikan, Long> {
+}
