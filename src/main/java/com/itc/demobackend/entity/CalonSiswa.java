32fd722c285b1fc5b2cf8f7aafc6dee880fbec21
nitanujabaturahmah <nitanujabaturahmah@gmail.com>
2023-01-20 14:42:34 +0700

    Initial commit

---
+++ src/main/java/com/itc/demobackend/entity/CalonSiswa.java
@@ -0,0 +1,25 @@
+package com.itc.demobackend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table
+public class CalonSiswa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String nama;
+
+    private String namaAyah;
+
+    private String noTelephone;
+
+    private String password;
+
+    private String email;
+}
---
+++ src/main/java/com/itc/demobackend/repository/CalonSiswaRepository.java
@@ -0,0 +1,8 @@
+package com.itc.demobackend.repository;
+
+import com.itc.demobackend.entity.CalonSiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalonSiswaRepository extends JpaRepository<CalonSiswa, Long> {
+
+}
